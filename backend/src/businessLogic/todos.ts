f45452bc9096bbc858b50b542d3ff17bdd5eabfd
bmk-star <mulmulber@gmail.com>
2023-03-20 02:54:58 +0300

    Get todo is implemented with request validation

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -12,6 +12,12 @@ const logger = createLogger('TodosAccess')
 const attachmentUtils = new AttachmentUtils()
 const todosAccess = new TodosAccess()
 
+//write get todos function
+export async function getTodosForUser(userId: string): Promise<TodoItem[]> {
+    logger.info('Get todos')
+    return todosAccess.getAllTodos(userId);
+  }
+
 //write create todo function
 export async function createTodo(
     newTodo: CreateTodoRequest,
